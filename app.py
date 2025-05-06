76c51d237d300b4f53300c3996c5af1a62908469
root <root@DESKTOP-C28NOQN.>
2025-05-07 00:00:32 +0900

    dockerコンテナでmysqlに接続（模索中）

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
 from flask import Flask
 import api.external as external
+import os
 
 def create_app():
 
     app = Flask(__name__)
-    USER='root'
-    PASSWORD='sqlpass'
-    HOST='localhost'
+    # USER='root'
+    # PASSWORD='sqlpass'
+    # HOST='localhost'
     # PORT = '3306'
-    DATABASE='calendar'
+    # DATABASE='calendar'
     # SQLALCHEMY_DATABASE_URI: str = 'mysql+pymysql://' + USER + ':' + PASSWORD + '@' + HOST + ':' + PORT + '/' + DATABASE + '?charset=utf8'
-    SQLALCHEMY_DATABASE_URI: str = 'mysql+pymysql://' + USER + ':' + PASSWORD + '@' + HOST + '/' + DATABASE + '?charset=utf8'
-
+    # SQLALCHEMY_DATABASE_URI: str = 'mysql+pymysql://' + USER + ':' + PASSWORD + '@' + HOST + '/' + DATABASE + '?charset=utf8'
+    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://docker:docker@mysql_db:3306/calendar'
     # DB設定を読み込む
-    app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+    # app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 
+    MYSQL_USER = os.environ.get('MYSQL_USER')
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
+    MYSQL_DATABASE = os.environ.get('MYSQL_DATABASE')
+    DB_HOST = 'db'  # Docker Compose の場合は 'db' を使用することもできます
+    DB_PORT = '3306'
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{DB_HOST}:{DB_PORT}/{MYSQL_DATABASE}'
     import api
     app.register_blueprint(api.calendar_app)
 
